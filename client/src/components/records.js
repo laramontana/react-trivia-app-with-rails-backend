aba7432ba06db2c8cc3e12ec87075012c8fafc59
laramontana <larabrightside@gmail.com>
2017-12-14 15:10:41 +0300

    Refactor Records component

--- client/src/components/records.js
+++ client/src/components/records.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchRecords } from '../actions/categoriesActions';
+import CategoryQuestion from '../containers/categoryQuestion';
+import Score from '../containers/score';
 
-const Records = ({ answer }) =>
-    <div>
-      <h2>Records</h2>
-    </div>
+import '../App.css';
 
-export default Records;
+class Records extends Component {
+
+  componentDidMount() {
+    this.props.fetchRecords(this.props.category.id)
+  }
+
+  render() {
+    const {records} = this.props
+    return (
+      <div className="App">
+        <h4 className="App-title">Records</h4>
+        {records.map(record=><h1>{record.user_name}</h4>)}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {records: state.records}
+}
+
+export default connect(mapStateToProps, {fetchRecords})(Records);
